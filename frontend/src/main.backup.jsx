29125f6ae548b36b15c93494ff5c73dc457b4f44
Githubtoken <harish.kamat01@gmail.com>
2026-08-18 23:17:30 +0530

    Add React frontend baseline

---
+++ frontend/src/main.backup.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+
+function App()
+{
+  return (
+
+    <h1>SKCP Frontend</h1>
+
+        );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+
+ <React.StrictMode>
+
+    <App />
+
+  </React.StrictMode>
+
+);
